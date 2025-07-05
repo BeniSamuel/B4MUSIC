231784e8426c489aee93317d8540603b6365d542
benisamuel <benisamuel566@gmail.com>
2025-07-05 16:45:07 +0200

    added hotlist components in home components

---
+++ src/Components/Home/Hotlist/Hotlist.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import hotlistIc from "../../../assets/Home/Hotlist/list-white.svg";
+import HotlistVid from "../HotlistVid/HotlistVid";
+import videos from "../../../data/HotlistVid/HotlistVid";
+
+const Hotlist: React.FC = () => {
+  return (
+    <div className=" flex flex-col gap-7 px-6 py-4 w-full">
+      <div className=" flex md:flex-row items-center gap-3">
+        <span className=" text-white font-semibold text-sm font-poppins">
+          HOTLIST
+        </span>
+        <img src={hotlistIc} className=" w-5 h-5 " />
+      </div>
+      <div className=" w-full overflow-x-auto">
+        <div className={` flex flex-row gap-12 w-fit py-2 px-1`}>
+          {videos.map((item) => {
+            return (
+              <HotlistVid
+                key={item.id}
+                cover={item.cover}
+                title={item.title}
+                id={item.id}
+                list={item.list}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hotlist;
