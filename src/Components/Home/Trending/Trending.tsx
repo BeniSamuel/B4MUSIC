6e2d84b2850bcd86ac701e88fca5a0e237b352b5
benisamuel <benisamuel566@gmail.com>
2025-07-05 16:46:37 +0200

    added trending component in home components

---
+++ src/Components/Home/Trending/Trending.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Video from "../../../data/TrendingVideo/TrendingVideo";
+import logo from "../../../assets/Home/Trending/arrow-logo.svg";
+import HotlistVid from "../HotlistVid/HotlistVid";
+
+const Trending: React.FC = () => {
+  return (
+    <div className=" flex-col w-full px-4 py-2">
+      <div className="flex flex-row h-24 w-48  items-center gap-3 ">
+        <span className=" text-[20px] font-semibold text-white font-poppins text-sm">
+          TRENDING
+        </span>
+        <img src={logo} />
+      </div>
+      <div className=" w-full  overflow-x-auto">
+        <div className="flex flex-row gap-12 py-2 px-1 w-fit">
+          {Video.map((item, index) => {
+            return (
+              <HotlistVid
+                key={index}
+                id={item.id}
+                cover={item.image}
+                title={item.title}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Trending;
