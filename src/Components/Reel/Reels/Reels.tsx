7a61db2b514c6ed929cbe35c04c76e4f03ae4bb2
benisamuel <benisamuel566@gmail.com>
2025-07-05 16:52:29 +0200

    added reels component to be used

---
+++ src/Components/Reel/Reels/Reels.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import like from "../../../assets/Reels/like.svg";
+import dislike from "../../../assets/Reels/dislike.svg";
+import comment from "../../../assets/Reels/comment.svg";
+import share from "../../../assets/Reels/share.svg";
+import More from "../../Common/More/More";
+
+
+type ReelsProps = {
+  image: string;
+}
+
+const Reels: React.FC<ReelsProps> = () => {
+  return (
+    <div className=" pb-6">
+      <div className=" flex flex-row gap-4">
+        <div className=" h-[30rem] w-[26rem] rounded-3xl bg-black">
+          <video controls className=" h-[30rem] w-[26rem] rounded-3xl">
+            <source />
+          </video>
+        </div>
+        <div className=" flex flex-col gap-20 items-center justify-center">
+          <img src={like} className=" h-5 w-5 cursor-pointer" />
+          <img src={dislike} className=" h-5 w-5 cursor-pointer" />
+          <img src={comment} className=" h-5 w-5 cursor-pointer" />
+          <img src={share} className=" h-5 w-5 cursor-pointer" />
+          <More/>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Reels;
