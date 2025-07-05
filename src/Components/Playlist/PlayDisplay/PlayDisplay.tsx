4990302def5d16443d1f662cb48c7dd0cfa4abb1
benisamuel <benisamuel566@gmail.com>
2025-07-05 16:48:01 +0200

    added playdisplay component in playlist components

---
+++ src/Components/Playlist/PlayDisplay/PlayDisplay.tsx
@@ -0,0 +1,25 @@
+// PlaylistDisplay.tsx
+import React, { useContext } from "react";
+import { PlayContext } from "../../../pages/Playlist/Playlist";
+
+const PlaylistDisplay: React.FC = () => {
+  const { playlists } = useContext(PlayContext);
+
+  return (
+    <div className=" overflow-y-auto">
+      {playlists.map((playlist, index) => (
+        <div key={index} className=" text-white p-4 flex flex-row gap-12">
+          <div className=" bg-[#171717] h-44 w-80 rounded-lg"></div>
+          <div className=" flex flex-col gap-1 justify-center">
+            <h2 className="text-sm font-poppins">{playlist.namePlaylist}</h2>
+            <p className=" font-poppins text-sm">
+              Created by: {playlist.nameCreator}
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PlaylistDisplay;
