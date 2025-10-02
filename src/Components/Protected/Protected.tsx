064ae88e7853828f6e1b9f34a30754aa9c116c44
Beni Samuel <149045020+BeniSamuel@users.noreply.github.com>
2025-10-02 12:08:47 +0200

    Update Protected.tsx for testing home

--- src/Components/Protected/Protected.tsx
+++ src/Components/Protected/Protected.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 const Protected: React.FC = () => {
   // const token = localStorage.getItem("authToken");
